f369c1461404ca36d60989898585a50912cd9f89
daimsharif <daimsharif@hotmail.com>
2023-09-07 14:30:47 +0400

    Feature B added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,7 @@
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-	}
-
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //featureB(); //remove comment tag to run function featureB
+    }
+    public static void featureB(){System.out.println("Hello, Mars!");}
 }
